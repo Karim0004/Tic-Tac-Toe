1330851a61f7ff94ac9336849f82177d526c8848
Karim <karimx099@gmail.com>
2022-06-13 15:40:12 +0200

    add player selection markup

--- script.js
+++ script.js
@@ -16,21 +16,60 @@ const gameboard = (() => {
 
     let getBoard = () => board;
 
-    return {insert, clear, getBoard}
+    return {insert, clear, getBoard};
 })();
 
 
-const game = () => {
+// module controlling the game //
+const game = (() => {
+    const players = [null, null];
+    let currentPlayer = 0;
+    let playing = false;
 
-}
+    let start = (player1, player2) => {
+        gameboard.clear();
+        players[0] = player1;
+        players[1] = player2;
+        playing = true;
+    }
+    
+    let turn = (cell) => {
+        if (playing) {
+            gameboard.insert(players[currentPlayer].symbol, cell);
+            (currentPlayer === 0) ? currentPlayer = 1 : currentPlayer = 0;
+        }
+    } 
+
+    return {start, turn}
+})();
 
 
-const displayController = () => {
+// module for viewing the board and getting player input through cell clicks //
+const displayController = ((gameboard) => {
+    
+    let displayCells = document.querySelectorAll('.board > *');
+    
+    // listening for clicks on cells //
+    displayCells.forEach((element) => {
+        element.addEventListener('click', (e) => {
+            game.turn(Number(e.target.id));
+            updateBoard();
+        })
+    })
 
-}
+    let updateBoard = () => {
+        const board = gameboard.getBoard()
+        for (let i in board) {
+            displayCells[i].textContent = board[i];
+        }
+    }
+
+    return {updateBoard};
+})(gameboard);
 
 
 const player = (name, symbol) => {
-    return {name, symbol}
+    if (!['X', 'O'].includes(symbol)) return console.log('ERORR: bad symbol');
+    return {name, symbol};
 }
 
