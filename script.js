3e653a589c254fe4ada39e724ade8116ee0e264d
Karim <karimx099@gmail.com>
2022-06-15 11:31:38 +0200

    functional play button play button creats players from form and starts the game

--- script.js
+++ script.js
@@ -26,21 +26,27 @@ const game = (() => {
     let currentPlayer = 0;
     let playing = false;
 
+    // starts the game //
     let start = (player1, player2) => {
         gameboard.clear();
         players[0] = player1;
         players[1] = player2;
         playing = true;
+        displayController.displayBoard();
     }
     
+    // takes a cell and plays a turn then switches current player for next turn //
     let turn = (cell) => {
         if (playing) {
             gameboard.insert(players[currentPlayer].symbol, cell);
             (currentPlayer === 0) ? currentPlayer = 1 : currentPlayer = 0;
+            displayController.updateBoard();
+
+            // TODO check if a player won //
         }
     } 
 
-    return {start, turn}
+    return {start, turn};
 })();
 
 
@@ -50,16 +56,34 @@ const displayController = (() => {
     let boardCells = document.querySelectorAll('.board > *');
     let winningScreen = document.getElementById('winning-screen');
     let startMenu = document.getElementById('start-menu');
+    let markButtons = document.querySelectorAll('.mark-buttons > button');
+    let startButton = document.getElementById('start');
+    let Names = document.querySelectorAll('.player-name');
 
     // listening for clicks on cells //
     boardCells.forEach((element) => {
         element.addEventListener('click', (e) => {
             game.turn(Number(e.target.id));
-            updateBoard();
+        })
+        })
+
+
+    // choosing mark buttons functionality //
+    markButtons.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            // removing selections that conflict with this selection //
+            for (let but of markButtons) {
+                if (but.textContent === button.textContent
+                    || but.parentElement === button.parentElement) {
+                    but.classList.remove('selected-mark');
+                }
+            }
+            // adding selection class to the clicked button //
+            button.classList.add('selected-mark');
         })
     })
 
-
+    // update the board from the gameboard object to the DOM //
     let updateBoard = () => {
         const board = gameboard.getBoard()
         for (let i in board) {
@@ -67,6 +91,18 @@ const displayController = (() => {
         }
     }
 
+    // starting the game by clicking on the play button //
+    startButton.addEventListener('click', () => {
+        if (markButtons[1].classList.contains('selected-mark')
+        || markButtons[2].classList.contains('selected-mark')) {
+            game.start(player(Names[0].value, 'O'), player(Names[1].value, 'X'));
+        } else {
+            game.start(player(Names[0].value, 'X'), player(Names[1].value, 'O'));
+        }
+    })
+
+
+    // methods for displaying game states //
     let displayBoard = () => {
         if (!startMenu.classList.contains('hidden')) {
             startMenu.classList.add('hidden');
@@ -98,15 +134,10 @@ const displayController = (() => {
     return {updateBoard, displayBoard, displayMenu, displayWinner};
 })();
 
+// player creator //
+const player = (name, symbol) => {
+    if (!['X', 'O'].includes(symbol)) return console.log('ERORR: bad symbol');
+    name = name || `player with ${symbol}`;
 
-// const player = (name, symbol) => {
-//     if (!['X', 'O'].includes(symbol)) return console.log('ERORR: bad symbol');
-//     return {name, symbol};
-// }
-
-// let buttons = document.querySelectorAll('.mark-buttons > button');
-// buttons.forEach((button) => {
-//     button.addEventListener('click', (e) => {
-//         e.target.classList.add('selected-mark');
-//     })
-// })
+    return {name, symbol};
+}
